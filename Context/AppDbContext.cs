ea2a5912f3f297903c7242e476146223d4dd368b
Leo <leosilvatanabi@gmail.com>
2023-02-22 10:43:28 -0300

    Add-Migration PedidoDetalhes

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LanchoneteMVC.Context
 {
+    //Adicioanr o DBContext sempre que adicionarmos o Model 
     //Classe de Contexto carrega informações de configurações e define quais as que estou mapiando para as tabelas
 
     public class AppDbContext : DbContext //Herdar  DbContext do EntityFrameworkCore
@@ -18,5 +19,10 @@ namespace LanchoneteMVC.Context
         public DbSet <Categoria> Categorias { get; set; }
         public DbSet <Lanche> Lanches { get; set; }
         public DbSet <CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+
+
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhes { get; set; }
+
     }
 }
