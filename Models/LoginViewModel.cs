62068d13ceea6f63bdd73742b26d6aa4ebe7d0d1
Leo <leosilvatanabi@gmail.com>
2023-02-23 14:07:55 -0300

    Implementario estrutra de Registro e Login

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchoneteMVC.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Informe o Usuário.")]
+        [Display(Name ="Usuário")]
+        public string UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Informe a Senha.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
+ 
